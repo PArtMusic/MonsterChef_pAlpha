09308d5cc075542dabb110c9f8532bfdfe336625
PArtMusic <PArtMusic91@gmail.com>
2017-05-24 21:14:45 -0400

    Added food, collision, and food movement

---
+++ MonsterChef/Assets/Scripts/FoodFloat.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodFloat : MonoBehaviour {
+    private bool move = true;
+    public Vector3 moveVector = Vector3.up;
+    public float MoveRange = 2.0f;
+    public float MoveSpeed = 0.5f;
+
+    private FoodFloat foodFloat;
+    private Vector3 startPosition;
+
+    private void Start()
+    {
+        foodFloat = this;
+        startPosition = foodFloat.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(move)
+        {
+            foodFloat.transform.position = startPosition + moveVector *
+                (MoveRange * Mathf.Sin(Time.timeSinceLevelLoad) * MoveSpeed);
+        }
+    }
+
+
+}
---
+++ MonsterChef/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Vector2 movement = new Vector2();
+    private Rigidbody2D rb2d_player;
+    public float speedMultiplier;
+
+    public void Start()
+    {
+        rb2d_player = GetComponent<Rigidbody2D>();
+    }
+
+    public void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        movement.x = moveHorizontal;
+        movement.y = moveVertical;
+        rb2d_player.AddForce(movement * speedMultiplier);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name.Contains("wall_"))
+        {
+            rb2d_player.velocity = Vector2.zero;
+        }
+    }
+}
